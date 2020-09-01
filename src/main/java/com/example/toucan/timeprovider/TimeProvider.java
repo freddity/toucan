4bb2a1bdd80fdd40256a5bb286c7cae21ad831c1
Ivan Iwanow <kubaice3@gmail.com>
2020-09-01 23:00:36 +0200

    TimeProvider is ready to use

--- src/main/java/com/example/toucan/timeprovider/TimeProvider.java
+++ src/main/java/com/example/toucan/timeprovider/TimeProvider.java
@@ -6,7 +6,9 @@ import java.util.Calendar;
 
 public class TimeProvider {
 
-    Calendar now = Calendar.getInstance();
-    DateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
-    int formattedDate = Integer.parseInt(formatter.format(now.getTime()));
+    public static String getTime() {
+        Calendar now = Calendar.getInstance();
+        DateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
+        return formatter.format(now.getTime());
+    }
 }
