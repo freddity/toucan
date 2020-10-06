cb76b08a4407a546029c1c63e9bbc96f1c62720b
Ivan Iwanow <kubaice3@gmail.com>
2020-10-06 18:04:24 +0200

    Commit of FilterSelfProfileActions

--- src/main/java/com/example/toucan/security/filters/FilterSelfProfileActions.java
+++ src/main/java/com/example/toucan/security/filters/FilterSelfProfileActions.java
@@ -1,5 +1,8 @@
 package com.example.toucan.security.filters;
 
+import com.example.toucan.model.dao.UserDetailsImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,12 +10,40 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
+@Component
 public class FilterSelfProfileActions extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("FILTER");
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        System.out.println("### FILTER ###");
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                if (headerNames.nextElement().matches("Bearer")) {
+                    System.out.println("### Bearer found ###");
+                }
+            }
+        }
+        System.out.println("2222");
+
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                System.out.println("Header: " + name + " value:" + request.getHeader(name));
+            }
+        }
+
+
+        System.out.println("33333");
+
+
+
+        chain.doFilter(request, response);
     }
+
+
 }
