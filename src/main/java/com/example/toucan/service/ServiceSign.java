fe631cc7d3a81e277fbec95a2fc3fa8f5588086f
Ivan Iwanow <kubaice3@gmail.com>
2020-10-12 00:40:16 +0200

    I've deleted unnecessary FilterSignUp.java, edited canUserBeLogged() in ServiceSign and found a way to throw http status and message to client

--- src/main/java/com/example/toucan/service/ServiceSign.java
+++ src/main/java/com/example/toucan/service/ServiceSign.java
@@ -10,19 +10,23 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @Service
 public class ServiceSign {
 
-    private final UserDetailsServiceImpl service;
+    private final UserDetailsServiceImpl detailsService;
     private final RepositoryUser repositoryUser;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
-    public ServiceSign(UserDetailsServiceImpl service, RepositoryUser repositoryUser, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
-        this.service = service;
+    public ServiceSign(UserDetailsServiceImpl detailsService, RepositoryUser repositoryUser, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.detailsService = detailsService;
         this.repositoryUser = repositoryUser;
         this.passwordEncoder = passwordEncoder;
         this.jwtUtil = jwtUtil;
@@ -40,16 +44,16 @@ public class ServiceSign {
     public String generateToken(DtoUsernamePassword dto) throws NullPointerException {
 
         if (canUserBeLogged(dto.getUsername(), dto.getPassword())) {
-            return jwtUtil.generateToken(service.loadUserByUsername(dto.getUsername()));
+            return jwtUtil.generateToken(detailsService.loadUserByUsername(dto.getUsername()));
         }
-        return null;
+        //todo I've found the way to throw error to client
+        throw new ResponseStatusException(FORBIDDEN, "username or password are incorrectly");
     }
 
+    //todo make checking if user does exists and other conditions
+    //TODO CHANGE USERNAME TO EMAIL EVERYWHERE
     private boolean canUserBeLogged(String username, String password) throws NullPointerException {
-
-        if (true) {
-            return true;
-        }
-        return false;
+        return detailsService.loadUserByUsername(username) != null
+                && detailsService.loadUserByUsername(username).getPassword().equals(password);
     }
 }
