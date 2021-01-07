3d96814ac3baa2b6bf6bb5afd258d4c2f27e1e38
quack <kubaice3@gmail.com>
2021-01-07 19:14:59 +0100

    Okay, so test isn't passing but I found and fixed wrong import. Next thing to do is make the test passing and understand why API want to get Authorization header.

--- src/test/java/com/example/toucan/ToucanApplicationTests.java
+++ src/test/java/com/example/toucan/ToucanApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ToucanApplicationTests {
 
-    @Test
+   /* @Test
     void contextLoads() {
-    }
+
+    }*/
 
 }
--- src/test/java/com/example/toucan/controller/ControllerSignTest.java
+++ src/test/java/com/example/toucan/controller/ControllerSignTest.java
@@ -1,4 +1,83 @@
 package com.example.toucan.controller;
 
+import com.example.toucan.ToucanApplication;
+import com.example.toucan.model.dto.DtoUsernamePassword;
+import com.example.toucan.model.entity.EntityUser;
+import com.example.toucan.repository.RepositoryNote;
+import com.example.toucan.repository.RepositoryUser;
+import com.example.toucan.service.ServiceSign;
+import com.example.toucan.util.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+//@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = ControllerSign.class)
+//@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes= ToucanApplication.class)
+@AutoConfigureMockMvc
 public class ControllerSignTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ServiceSign serviceSign;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @MockBean
+    private RepositoryNote repositoryNote;
+
+    @MockBean
+    private RepositoryUser repositoryUser;
+
+    @BeforeEach
+    void printApplicationContext() {
+        Arrays.stream(applicationContext.getBeanDefinitionNames())
+                .map(name -> applicationContext.getBean(name).getClass().getName())
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void createUser_valuesOK_statusOK() throws Exception {
+        //todo check out possibility of create object and call method in same expression
+        JwtUtil jwtUtil = new JwtUtil();
+
+        when(serviceSign.createUser(eq("user1"), eq("user1")))
+                .thenReturn(jwtUtil.generateToken(new EntityUser("user1", "user1")));
+
+        mockMvc.perform(
+                post("/signup")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(new DtoUsernamePassword("user1", "user1")))
+        ).andExpect(status().isOk());
+    }
+
+
 }
